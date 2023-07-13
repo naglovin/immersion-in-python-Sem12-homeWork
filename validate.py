7ae429d91ca203060532391485f5045eb4e960b3
Danil <Kutfilloev.danil@yandex.ru>
2023-07-13 11:40:31 +0500

    основная работа в task и students

---
+++ validate.py
@@ -0,0 +1,18 @@
+class Validate:
+    "Дескриптор на проверку в ФИО наличия первой заглавной буквы, а также что ФИО состоит только из букв"
+
+    def __set_name__(self, owner, name):
+        self.param_name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.param_name)
+
+    def __set__(self, instance, value):
+        self.validate(value)
+        setattr(instance, self.param_name, value)
+
+    def validate(self, value):
+        if not value.isalpha():
+            raise TypeError(f'Значение {value} должно содержать только буквы')
+        if not value.istitle():
+            raise TypeError(f'Значение {value} должно начинаться с заглавной буквы')
